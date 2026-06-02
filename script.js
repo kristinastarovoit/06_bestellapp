3326fbd4f71e8e88025892511d53df575aae292a
kristina <kristina.starovoit@gmail.com>
2026-06-02 15:48:57 +0200

    refactor changed order of js functions

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function init() {
     renderAllDishes();
     renderCart();
-    checkIfCartIsEmpty();
+    updateCartState();
 }
 
 function renderAllDishes() {
@@ -17,62 +17,6 @@ function renderCategoryDishes(contentId, categoryName) {
         if (dishes[indexDish]["category"] == categoryName) {
             dishRef.innerHTML += getDishTemplate(indexDish);
         }
-    }
-}
-
-function addOneItemToCart(indexDish) {
-    dishes[indexDish].amount++;
-    toggleCartButtonAmount(indexDish);
-    checkIfCartIsEmpty();
-    renderCart();
-}
-
-function toggleCartButtonAmount(indexDish) {
-    let cartButton = document.getElementById(`basket_button_${indexDish}`);
-    let dishText = document.getElementById(`dish_description_${indexDish}`);
-    if (dishes[indexDish].amount == 0) {
-        dishText.classList.remove('move_dish_text');
-        cartButton.classList.remove('orange_font');
-        cartButton.innerText = `Add to Basket`;
-    } else {
-        dishText.classList.add('move_dish_text');
-        cartButton.classList.add('orange_font');
-        cartButton.innerText = `Added ${dishes[indexDish].amount}`;
-    }
-}
-
-function removeOneItemFromCart(indexDish) {
-    dishes[indexDish].amount--;
-    if (dishes[indexDish].amount < 0) {
-        dishes[indexDish].amount = 0;
-    }
-    console.log(dishes[indexDish].amount);
-    toggleCartButtonAmount(indexDish);
-    checkIfCartIsEmpty();
-    renderCart();
-}
-
-function deleteFromCart(indexDish) {
-    dishes[indexDish].amount = 0;
-    toggleCartButtonAmount(indexDish);
-    checkIfCartIsEmpty();
-    renderCart();
-}
-
-function isAmountZero(element) {
-    return element.amount === 0;
-}
-
-function checkIfCartIsEmpty() {
-    const cartTotalRef = document.getElementById('cart_total_content');
-    const cartEmptyText = document.getElementById('empty_cart');
-    if (dishes.every(isAmountZero) == true) {
-        cartTotalRef.classList.add('d_none');
-        cartEmptyText.classList.remove('d_none');
-    }
-    else {
-        cartTotalRef.classList.remove('d_none');
-        cartEmptyText.classList.add('d_none');
     }
 }
 
@@ -95,6 +39,31 @@ function renderCartTotal() {
     const totalRef = document.getElementById('total');
     subtotalRef.innerText = formatToCurrency(calculateSubtotal());
     totalRef.innerText = formatToCurrency(calculateTotal());
+}
+
+function addOneItemToCart(indexDish) {
+    dishes[indexDish].amount++;
+    toggleCartButtonAmount(indexDish);
+    updateCartState();
+    renderCart();
+}
+
+function removeOneItemFromCart(indexDish) {
+    dishes[indexDish].amount--;
+    if (dishes[indexDish].amount < 0) {
+        dishes[indexDish].amount = 0;
+    }
+    console.log(dishes[indexDish].amount);
+    toggleCartButtonAmount(indexDish);
+    updateCartState();
+    renderCart();
+}
+
+function deleteFromCart(indexDish) {
+    dishes[indexDish].amount = 0;
+    toggleCartButtonAmount(indexDish);
+    updateCartState();
+    renderCart();
 }
 
 function calculateItemSum(indexDish) {
@@ -123,6 +92,20 @@ function formatToCurrency(price) {
     });
 }
 
+function toggleCartButtonAmount(indexDish) {
+    let cartButton = document.getElementById(`basket_button_${indexDish}`);
+    let dishText = document.getElementById(`dish_description_${indexDish}`);
+    if (dishes[indexDish].amount == 0) {
+        dishText.classList.remove('move_dish_text');
+        cartButton.classList.remove('orange_font');
+        cartButton.innerText = `Add to Basket`;
+    } else {
+        dishText.classList.add('move_dish_text');
+        cartButton.classList.add('orange_font');
+        cartButton.innerText = `Added ${dishes[indexDish].amount}`;
+    }
+}
+
 function openDialog() {
     const dialogRef = document.getElementById('order_dialog');
     dialogRef.showModal();
@@ -137,17 +120,34 @@ function closeDialog() {
     resetAllAmounts();
 }
 
+function resetAllAmounts() {
+    emptyCart();
+    updateCartState();
+    renderCart();
+    renderAllDishes();
+}
+
 function emptyCart() {
     for (indexDish = 0; indexDish < dishes.length; indexDish++) {
         dishes[indexDish].amount = 0;
     }
 }
 
-function resetAllAmounts() {
-    emptyCart();
-    checkIfCartIsEmpty();
-    renderCart();
-    renderAllDishes();
+function isAmountZero(element) {
+    return element.amount === 0;
+}
+
+function updateCartState() {
+    const cartTotalRef = document.getElementById('cart_total_content');
+    const cartEmptyText = document.getElementById('empty_cart');
+    if (dishes.every(isAmountZero) == true) {
+        cartTotalRef.classList.add('d_none');
+        cartEmptyText.classList.remove('d_none');
+    }
+    else {
+        cartTotalRef.classList.remove('d_none');
+        cartEmptyText.classList.add('d_none');
+    }
 }
 
 function showCartMobile() {
